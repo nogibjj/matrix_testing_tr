35daf5bc9452304312a0b32cbe691c1130c80d0d
Tianji Rao <rao@TTRao.local>
2023-09-20 14:55:34 -0400

    change files

--- test_main.py
+++ test_main.py
@@ -8,7 +8,6 @@ import pandas as pd
 def test_desc_df():
     df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
     # mean
-    # print(desc_df(df).loc["mean", "Electric Range"], desc_df(df).loc["50%", "Model Year"], desc_df(df).loc["std", "Electric Range"])
     assert desc_df(df).loc["mean", "Electric Range"] == 70.49573804284242
     # median
     assert desc_df(df).loc["50%", "Model Year"] == 2021.0
@@ -18,8 +17,6 @@ def test_desc_df():
 
 def test_bar_plot():
     df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
-    # Here we can focus on the visualization of Electric Range using a bar plot
-    # Since the data set is too large to run in codespace, here we only use the first 100 observation as an example
     bar_plot(df["Electric Range"][:100])
 
 
